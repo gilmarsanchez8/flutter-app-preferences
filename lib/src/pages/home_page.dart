b08a0dd0cb5f6fd83cc870f28e1f91c8467a0b06
Gilmar Sanchez Cuartas <gsanchez@tsgroup.local>
2021-05-08 16:43:36 -0500

    Crear menu y navegación entre páginas

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app_preferences/src/pages/settings_page.dart';
 
 class HomePage extends StatelessWidget {
   static final String routeName = 'home';
@@ -8,6 +9,7 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Preferencias de usuario'),
       ),
+      drawer: _crearMenu(context),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
@@ -17,6 +19,46 @@ class HomePage extends StatelessWidget {
           Divider(),
           Text('Nombre de usuario'),
           Divider(),
+        ],
+      ),
+    );
+  }
+
+  Drawer _crearMenu(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          DrawerHeader(
+            child: Container(),
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage('assets/menu-img.jpg'),
+                    fit: BoxFit.cover)),
+          ),
+          ListTile(
+            leading: Icon(Icons.home, color: Colors.blue),
+            title: Text('Home'),
+            onTap: () => Navigator.pushReplacementNamed(context, HomePage.routeName),
+          ),
+          ListTile(
+            leading: Icon(Icons.party_mode, color: Colors.blue),
+            title: Text('Party mode'),
+            onTap: () {},
+          ),
+          ListTile(
+            leading: Icon(Icons.people, color: Colors.blue),
+            title: Text('People'),
+            onTap: () {},
+          ),
+          ListTile(
+            leading: Icon(Icons.settings, color: Colors.blue),
+            title: Text('Settings'),
+            onTap: () => {
+              //Navigator.pop(context),
+              Navigator.pushReplacementNamed(context, SettingsPage.routeName),
+            }
+          ),
         ],
       ),
     );
